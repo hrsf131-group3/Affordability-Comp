bbceb3bb798b18eb39828c01e8ce54dd6c8c9d77
Blake-Whitham <bwhitham77@gmail.com>
2020-11-12 19:09:04 -0800

    Added style object

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/extensions
 import MortgageCalc from './mortageCalc.jsx';
 
-ReactDOM.render(<MortgageCalc />, document.getElementById('app'))
+ReactDOM.render(<MortgageCalc />, document.getElementById('app'));
--- client/src/numberCruncher.js
+++ client/src/numberCruncher.js
@@ -1,34 +1,31 @@
-//import from db eventually
-var homePrice = 1200000;
-var downPaymentPercent = .2;
-var interestRate = .0292;
-var downPayment = downPaymentPercent * homePrice;
-var monthlyRate = interestRate/12;
-var loanLife = 30;
-var paymentsInLoanLife= loanLife * 12;
+// import from db eventually
+const homePrice = 1200000;
+const downPaymentPercent = 0.2;
+const interestRate = 0.0292;
+const downPayment = downPaymentPercent * homePrice;
+const monthlyRate = interestRate / 12;
+const loanLife = 30;
+const paymentsInLoanLife = loanLife * 12;
+const firstEQ = ((homePrice - downPayment) * monthlyRate);
 
-var segment = 1 + (Math.pow(monthlyRate, paymentsInLoanLife));
-var total = segment / (segment - 1);
+const payment = Math.round(firstEQ / (1 - (1 / ((1 + monthlyRate) ** paymentsInLoanLife))));
 
-var payment = Math.round(((homePrice - downPayment) * monthlyRate) / (1 - (1 / Math.pow((1+monthlyRate), paymentsInLoanLife ))));
+let mortageInsurance = 0;
 
-var mortageInsurance = 0;
-
-if (downPaymentPercent < .2) {
-  mortageInsurance = 356 + ((.2 - downPaymentPercent) * 100)
+if (downPaymentPercent < 0.2) {
+  mortageInsurance = 356 + ((0.2 - downPaymentPercent) * 100);
 }
 
-
-var math = {
-  homePrice: homePrice, // pulled from DB once in place
+const math = {
+  homePrice, // pulled from DB once in place
   downPayment: downPaymentPercent * homePrice,
-  interestRate: interestRate,
-  payment: payment,
+  interestRate,
+  payment,
   principalInterest: payment,
-  propertyTax: Math.round(payment * .16724),
+  propertyTax: Math.round(payment * 0.16724),
   homeInsurance: 75,
-  mortageInsurance: mortageInsurance
-}
+  mortageInsurance,
+};
 // console.log(math)
 
-export default math
+export default math;
