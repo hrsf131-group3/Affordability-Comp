f152b66318e836ed4a1024e7dae9d8a1eb1eb2a5
Blake-Whitham <bwhitham77@gmail.com>
2020-11-15 11:01:16 -0800

    testing v8

--- specs/seed.test.js
+++ specs/seed.test.js
@@ -4,22 +4,10 @@
 /* eslint-disable array-callback-return */
 /* eslint-disable no-undef */
 const mongoose = require('mongoose');
-// const mongo = require('../DataBase/mongo.js');
-// const seed = require('../seed');
 
 describe('drop', () => {
   let connection;
   let db;
-  // beforeAll(async () => {
-  //   connection = await mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
-  //   db = await mongoose.connection;//  tried removing async
-  //   await db.collection('testing').deleteMany({});
-  //   console.log('opened connection');
-  // });
-  // afterAll(async () => {
-  //   console.log('closed connection');
-  //   await mongoose.connection.close();
-  // });
 
   it('should seed DB', async () => {
     connection = await mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
