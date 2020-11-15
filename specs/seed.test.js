5a3e51a952efab7acad2a5e07675ae79da2af3f4
Blake-Whitham <bwhitham77@gmail.com>
2020-11-15 14:53:26 -0800

    updated seed test

--- specs/seed.test.js
+++ specs/seed.test.js
@@ -4,15 +4,9 @@
 
 const axios = require('axios');
 
-const seed = require('../seed');
 // const mongo = require('../DataBase/mongo');
 
 describe('API to DB', () => {
-  beforeAll(async () => {
-    await seed.drop();
-    await seed.DataGen();
-  });
-
   it('should receive data from DB through API call', () => {
     axios.get('/dbs')
       .then((data) => expect(data.length).toEqual(100));
