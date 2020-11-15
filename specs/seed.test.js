4657cc55ac2ea6bf5b12bb5295197250325fbf66
Blake-Whitham <bwhitham77@gmail.com>
2020-11-15 10:58:01 -0800

    test v7

--- specs/seed.test.js
+++ specs/seed.test.js
@@ -10,23 +10,31 @@ const mongoose = require('mongoose');
 describe('drop', () => {
   let connection;
   let db;
-  beforeAll(async () => {
+  // beforeAll(async () => {
+  //   connection = await mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
+  //   db = await mongoose.connection;//  tried removing async
+  //   await db.collection('testing').deleteMany({});
+  //   console.log('opened connection');
+  // });
+  // afterAll(async () => {
+  //   console.log('closed connection');
+  //   await mongoose.connection.close();
+  // });
+
+  it('should seed DB', async () => {
     connection = await mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
     db = await mongoose.connection;//  tried removing async
     await db.collection('testing').deleteMany({});
     console.log('opened connection');
-  });
-  afterAll(async () => {
-    console.log('closed connection');
-    await mongoose.connection.close();
-  });
 
-  it('should seed DB', async () => {
     const testing = db.collection('testing');
 
     const mock = { id: 2, homePrice: 666 };
     await testing.insertOne(mock);
     const inserted = await testing.findOne({ homePrice: 666 });
     expect(inserted).toEqual(mock);
+
+    console.log('closed connection');
+    await mongoose.connection.close();
   });
 });
