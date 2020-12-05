6af81876215ec0bdc9b3f2c137b65b21eb5dce5d
ecv2115 <ecv.eren@gmail.com>
2020-12-05 02:05:26 -0500

    updated mongo schema

--- DataBase/schema.js
+++ DataBase/schema.js
@@ -1,24 +1,21 @@
 const mongoose = require('mongoose');
 
-const PriceModel = mongoose.model('Price', new mongoose.Schema({ id: Number, homePrice: Number }));
-
-const AgentModel = mongoose.model('Agent', new mongoose.Schema({
-  name: String,
-  title: String,
-  phone: String,
-  rating: Number,
-  sales: Number,
+const homeModel = mongoose.model('Home', new mongoose.Schema({
+  "askingPrice": Number,
+  "hoa": Number,
+  "insurance": Number,
+  "taxAndAssesment": [{
+       "year": Date,
+       "tax": Number,
+       "assesment": Number
+  }],
+  "priceHistory": [{
+       "date": Date,
+       "price": Number,
+       "event": String
+  }],
+  "neighborhood": {
+       "zip": Number, 
+       "interestRate": Number
+  }
 }));
-const ScheduleModel = mongoose.model('Schedule', new mongoose.Schema({
-  in_person: Boolean,
-  date: String,
-  time: String,
-  name: String,
-  phone: String,
-  email: String,
-  financing: Boolean,
-}));
-
-module.exports.Price = PriceModel;
-module.exports.Agent = AgentModel;
-module.exports.Schedule = ScheduleModel;
