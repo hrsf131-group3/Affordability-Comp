ffde709bb7c878284d8e0ead744e2bef03d90b42
ecv2115 <ecv.eren@gmail.com>
2020-12-17 00:33:22 -0500

    reverted back to previous schema

--- DataBase/schema.js
+++ DataBase/schema.js
@@ -1,44 +1,44 @@
 const mongoose = require('mongoose');
 
-// const ListingSchema = new mongoose.Schema({
-//      "listing_id": Number,
-//      "price": Number,
-//      "hoa": Number,
-//      "insurance": Number,
-//      "taxAndAssesment": {
-//           "year": Date,
-//           "tax": Number,
-//           "assesment": Number
-//      },
-//      "priceHistory": [{
-//           "date": Date,
-//           "price": Number,
-//           "event": String
-//      }],
-//      "neighborhood": {
-//           "zip": Number, 
-//           "interestRate": Number
-//      }
-// });
-
-module.exports = mongoose.model('Listing', ListingSchema)
-
 const ListingSchema = new mongoose.Schema({
-     listing_id:{type:Number, required: true, unique: true},
-     price:{type:Number, required: true},
-     hoa: {type: Number, required: true},
-     insurance: {type: Number, required: true},
-     taxes: {
-          year: {type: Number},
-          tax: {type: Number},
-          assesment: {type: Number}
+     "listing_id": Number,
+     "price": Number,
+     "hoa": Number,
+     "insurance": Number,
+     "taxAndAssesment": {
+          "year": Date,
+          "tax": Number,
+          "assesment": Number
      },
-     priceHistory: [
-          {
-               year: {type: Number}, price: {type: Number}, event: {type: String}
-          }
-     ],
-     neighborhood: {
-          zip: {type: Number}, interestRate: {type: Number}
+     "priceHistory": [{
+          "date": Date,
+          "price": Number,
+          "event": String
+     }],
+     "neighborhood": {
+          "zip": Number, 
+          "interestRate": Number
      }
 });
+
+// module.exports = mongoose.model('Listing', ListingSchema)
+
+// const ListingSchema = new mongoose.Schema({
+//      listing_id:{type:Number, required: true, unique: true},
+//      price:{type:Number, required: true},
+//      hoa: {type: Number, required: true},
+//      insurance: {type: Number, required: true},
+//      taxes: {
+//           year: {type: Number},
+//           tax: {type: Number},
+//           assesment: {type: Number}
+//      },
+//      priceHistory: [
+//           {
+//                year: {type: Number}, price: {type: Number}, event: {type: String}
+//           }
+//      ],
+//      neighborhood: {
+//           zip: {type: Number}, interestRate: {type: Number}
+//      }
+// });
